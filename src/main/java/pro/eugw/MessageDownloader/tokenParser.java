79992381e4d83fdc19ff3d1636faadf88cd98e8b
EugW <wug.bond@gmail.com>
2017-08-21 20:23:15 +0500

    TEST2 build server

--- src/main/java/pro/eugw/MessageDownloader/tokenParser.java
+++ src/main/java/pro/eugw/MessageDownloader/tokenParser.java
@@ -48,7 +48,7 @@ class tokenParser {
         }
         FileWriter fileWriter = new FileWriter(config);
         properties.store(fileWriter, "TOKENS");
-        System.out.println("TEST");
+        System.out.println("TEST2");
         return list;
     }
 
